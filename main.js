a4eb0d9a34ca1cd501324c87c1eb6cd267569955
Alice Hong <alicehon@andrew.cmu.edu>
2022-03-22 19:32:25 -0400

    Made all appointment listings buttons onload.

--- main.js
+++ main.js
@@ -7,17 +7,18 @@ function Appointment(time, date, type, location) {
 }
 
 // build appointment list
-var apptList = [Appointment("8:50 AM", "Sunday, February 27, 2022", "Asymptomatic COVID-19 Test", "TCS Hall"),
-                Appointment("9:00 AM", "Monday, February 28, 2022", "Asymptomatic COVID-19 Test", "TCS Hall"),
-                Appointment("8:50 AM", "Tuesday, March 1, 2022", "Asymptomatic COVID-19 Test", "TCS Hall"),
-                Appointment("8:55 AM", "Tuesday, March 1, 2022", "Asymptomatic COVID-19 Test", "TCS Hall"),
-                Appointment("3:20 PM", "Tuesday, March 1, 2022", "Asymptomatic COVID-19 Test", "TCS Hall"),
-                Appointment("2:30 PM", "Thursday, March 3, 2022", "Asymptomatic COVID-19 Test", "TCS Hall"),
-                Appointment("2:40 PM", "Thursday, March 3, 2022", "Asymptomatic COVID-19 Test", "TCS Hall")]
+var apptList = [new Appointment("8:50 AM", "Sunday, February 27, 2022", "Asymptomatic COVID-19 Test", "TCS Hall"),
+                new Appointment("9:00 AM", "Monday, February 28, 2022", "Asymptomatic COVID-19 Test", "TCS Hall"),
+                new Appointment("8:50 AM", "Tuesday, March 1, 2022", "Asymptomatic COVID-19 Test", "TCS Hall"),
+                new Appointment("8:55 AM", "Tuesday, March 1, 2022", "Asymptomatic COVID-19 Test", "TCS Hall"),
+                new Appointment("3:20 PM", "Tuesday, March 1, 2022", "Asymptomatic COVID-19 Test", "TCS Hall"),
+                new Appointment("2:30 PM", "Thursday, March 3, 2022", "Asymptomatic COVID-19 Test", "TCS Hall"),
+                new Appointment("2:40 PM", "Thursday, March 3, 2022", "Asymptomatic COVID-19 Test", "TCS Hall")]
+
 
 function loadAppointmentListings() {
-  console.log(apptList);
-  // for(let i = 0; i < apptList.length; i++) {
-  //   document.getElementById("appointment_listings").innerHTML = 
-  // }
+  for(let i = 0; i < apptList.length; i++) {
+    document.getElementById("days").innerHTML += 
+      '<td class="button">' + '<a href="appointment-details.html">' + apptList[i].time + '</a></td>'
+  }
 }
